e42184ff0c953d85b301e16f32031afdb0929e89
bluberrywaffl <127812144+bluberrywaffl@users.noreply.github.com>
2023-04-11 11:27:59 +0900

    commit

---
+++ src/pages/count.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import styles from "@/styles/Counter.module.css";
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <h1>
+                Count: {/* count 가 0보다 작으면 minus 클래스 적용. */}
+                {count < 0 ? <span className={styles.minus}>{count}</span> : count} 
+            </h1>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+    );
+}
+
--- src/pages/world.js
+++ src/pages/world.js
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function World(props) {
-    return <span style={{ fontWeight: "bold"}}>
-        {props.name}!
-    </span>;
+    return <span style={{ fontWeight: "bold"}}>{props.name}!</span>;
 }//f
